c47df139639da1ee931a24053b7dd304ede0b812
LuthfiLN <nurrahmanluthfi15@gmail.com>
2026-05-18 17:08:02 +0700

    Add files via upload

---
+++ backup_minimarket_maju_bersama.sql
@@ -0,0 +1,222 @@
+-- MariaDB dump 10.19  Distrib 10.4.28-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: minimarket_maju_bersama
+-- ------------------------------------------------------
+-- Server version	10.4.28-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+DROP TABLE IF EXISTS `detail_transaksi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `detail_transaksi` (
+  `id_detail` int(11) NOT NULL AUTO_INCREMENT,
+  `id_transaksi` int(11) DEFAULT NULL,
+  `id_produk` int(11) DEFAULT NULL,
+  `jumlah` int(11) NOT NULL,
+  `subtotal` decimal(12,2) NOT NULL,
+  PRIMARY KEY (`id_detail`),
+  KEY `id_transaksi` (`id_transaksi`),
+  KEY `id_produk` (`id_produk`),
+  CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE,
+  CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+LOCK TABLES `detail_transaksi` WRITE;
+/*!40000 ALTER TABLE `detail_transaksi` DISABLE KEYS */;
+INSERT INTO `detail_transaksi` VALUES (1,1,1,1,75000.00),(2,1,2,1,34000.00),(3,2,1,1,75000.00),(4,3,6,10,35000.00),(5,4,3,1,16000.00),(6,4,1,1,75000.00);
+/*!40000 ALTER TABLE `detail_transaksi` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER update_stok_setelah_jual
+AFTER INSERT ON detail_transaksi
+FOR EACH ROW
+BEGIN
+   UPDATE produk SET stok = stok - NEW.jumlah
+   WHERE id_produk = NEW.id_produk;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Table structure for table `karyawan`
+--
+
+DROP TABLE IF EXISTS `karyawan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `karyawan` (
+  `id_karyawan` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_user` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','kasir') NOT NULL,
+  PRIMARY KEY (`id_karyawan`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+LOCK TABLES `karyawan` WRITE;
+/*!40000 ALTER TABLE `karyawan` DISABLE KEYS */;
+INSERT INTO `karyawan` VALUES (1,'Luthfi','luthfi_admin','admin2297','admin'),(2,'Rido','rido_kasir','pass135','kasir'),(3,'Mikael','mikael_kasir','pass246','kasir'),(4,'Awan','awan_kasir','pass789','kasir');
+/*!40000 ALTER TABLE `karyawan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kategori`
+--
+
+DROP TABLE IF EXISTS `kategori`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_kategori` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_kategori`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kategori`
+--
+
+LOCK TABLES `kategori` WRITE;
+/*!40000 ALTER TABLE `kategori` DISABLE KEYS */;
+INSERT INTO `kategori` VALUES (1,'Makanan'),(2,'Minuman'),(3,'Kebutuhan Pokok');
+/*!40000 ALTER TABLE `kategori` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `laporan_penjualan`
+--
+
+DROP TABLE IF EXISTS `laporan_penjualan`;
+/*!50001 DROP VIEW IF EXISTS `laporan_penjualan`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `laporan_penjualan` AS SELECT
+ 1 AS `tanggal_transaksi`,
+  1 AS `nama_kasir`,
+  1 AS `nama_produk`,
+  1 AS `jumlah`,
+  1 AS `subtotal` */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Table structure for table `produk`
+--
+
+DROP TABLE IF EXISTS `produk`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produk` (
+  `id_produk` int(11) NOT NULL AUTO_INCREMENT,
+  `id_kategori` int(11) DEFAULT NULL,
+  `nama_produk` varchar(150) NOT NULL,
+  `harga` decimal(10,2) NOT NULL,
+  `stok` int(11) NOT NULL,
+  PRIMARY KEY (`id_produk`),
+  KEY `id_kategori` (`id_kategori`),
+  CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produk`
+--
+
+LOCK TABLES `produk` WRITE;
+/*!40000 ALTER TABLE `produk` DISABLE KEYS */;
+INSERT INTO `produk` VALUES (1,3,'Beras Kelinci 5Kg',75000.00,47),(2,3,'Minyak Goreng 2L',34000.00,29),(3,3,'Gula Pasir 1Kg',16000.00,39),(4,2,'Aqua 600ml',4000.00,100),(5,2,'Teh Kotak 300ml',3000.00,60),(6,1,'Indomie Goreng',3500.00,40),(7,1,'Keripik Kentang',6000.00,40),(8,1,'Biskuit',10000.00,60);
+/*!40000 ALTER TABLE `produk` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaksi`
+--
+
+DROP TABLE IF EXISTS `transaksi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL AUTO_INCREMENT,
+  `tanggal_transaksi` datetime DEFAULT current_timestamp(),
+  `id_karyawan` int(11) DEFAULT NULL,
+  `total_harga` decimal(12,2) DEFAULT 0.00,
+  PRIMARY KEY (`id_transaksi`),
+  KEY `id_karyawan` (`id_karyawan`),
+  CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+LOCK TABLES `transaksi` WRITE;
+/*!40000 ALTER TABLE `transaksi` DISABLE KEYS */;
+INSERT INTO `transaksi` VALUES (1,'2026-05-14 10:30:00',2,109000.00),(2,'2026-05-15 01:24:01',1,75000.00),(3,'2026-05-16 13:44:58',3,35000.00),(4,'2026-05-16 14:38:05',4,91000.00);
+/*!40000 ALTER TABLE `transaksi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Final view structure for view `laporan_penjualan`
+--
+
+/*!50001 DROP VIEW IF EXISTS `laporan_penjualan`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `laporan_penjualan` AS select `t`.`tanggal_transaksi` AS `tanggal_transaksi`,`k`.`nama_user` AS `nama_kasir`,`p`.`nama_produk` AS `nama_produk`,`dt`.`jumlah` AS `jumlah`,`dt`.`subtotal` AS `subtotal` from (((`transaksi` `t` join `karyawan` `k` on(`t`.`id_karyawan` = `k`.`id_karyawan`)) join `detail_transaksi` `dt` on(`t`.`id_transaksi` = `dt`.`id_transaksi`)) join `produk` `p` on(`dt`.`id_produk` = `p`.`id_produk`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-16 15:19:50
